3bbc34558bec929f74d11a9540398ef4f5038ee9
mistrydhrumi <mistrydhrumi9134@gmail.com>
2026-06-09 15:50:23 +0530

    [fix] : link file

--- components/ui/link.tsx
+++ components/ui/link.tsx
@@ -1,12 +1,20 @@
 import Link from "next/link";
+
+type LinkNormalProps = {
+    href: string;
+    name: string;
+    className?: string;
+    target?: string;
+};
+
 export default function LinkNormal({
     href,
     name,
     className = "link",
-    targrt = "_self",
-}) {
+    target = "_self",
+}: LinkNormalProps) {
     return (
-        <Link href={href} className={className} target={targrt}>
+        <Link href={href} className={className} target={target}>
             {name}
         </Link>
     );
