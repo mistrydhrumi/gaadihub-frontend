9c105dcd75272fc31787f31cc18ddb3e003155a5
mistrydhrumi <mistrydhrumi9134@gmail.com>
2026-06-12 16:00:55 +0530

    [fix] : Cars-card

--- components/cars/CarCard.tsx
+++ components/cars/CarCard.tsx
@@ -5,6 +5,10 @@ import {
     CardContent,
 } from "@/components/ui/card";
 import { Badge } from "../ui/badge";
+import { Button } from "../ui/button";
+import { MapPin } from "lucide-react";
+import { ThumbsUp } from "lucide-react";
+import { ThumbsDown } from "lucide-react";
 
 export default function CarCard({ car }: any) {
     return (
@@ -17,40 +21,67 @@ export default function CarCard({ car }: any) {
                     height={300}
                     className="w-full h-52 object-cover border-b-gray-200"
                 />
+                <Badge
+                    variant="secondary"
+                    className="rounded-full px-2.5 py-1 text-xs font-medium shadow-md absolute top-2 right-2"
+                >
+                    {car.badge}
+                </Badge>
                 <CardContent className="p-4">
-                    <div className="flex items-start justify-between gap-3">
-                        <h3 className="text-lg font-semibold leading-tight">
+                    <div className="flex flex-row items-start justify-between gap-3">
+                        <h3 className=" text-xl font-bold leading-tight text-slate-900 pb-2">
                             {car.brand} {car.model}
                         </h3>
-                        <Badge
-                            variant="default"
-                            className="rounded-full px-2.5 py-1 text-xs font-medium shadow-md"
-                        >
-                            {car.badge}
-                        </Badge>
+                        <div className="shrink-0 text-right overflow-hidden">
+                            <div className="flex flex-col items-end">
+                                <div className="flex items-center gap-1">
+                                    <span className="text-sm font-medium text-green-600">
+                                        Save ₹
+                                        {(
+                                            (car.original_price - car.discount_price) /
+                                            1000
+                                        ).toFixed(0)}
+                                        K
+                                    </span>
+
+                                    <span className="text-sm text-slate-400 line-through">
+                                        ₹{(car.original_price / 100000).toFixed(2)}L
+                                    </span>
+                                </div>
+
+                                <p className="text-2xl font-bold text-slate-900 leading-none">
+                                    ₹{(car.discount_price / 100000).toFixed(2)}L
+                                </p>
+                            </div>
+                        </div>
                     </div>
-                    <div className="flex flex-wrap items-center gap-2 text-sm text-slate-600">
-                        <Badge variant="outline" className="rounded-full px-2.5 py-1">
-                            {car.registration_year}
+
+
+                    <div className="mt-1 flex items-center gap-2 text-sm text-slate-600">
+                        <Badge variant="outline" className="text-xs rounded-full px-2.5 py-1">
+                            {car.km_driven.toLocaleString()} kms
                         </Badge>
-                        <Badge variant="outline" className="rounded-full px-2.5 py-1">
-                            {car.fuel_type}
-                        </Badge>
-                        <Badge variant="outline" className="rounded-full px-2.5 py-1">
+                        <Badge variant="outline" className="text-xs rounded-full px-2.5 py-1">
                             {car.transmission}
                         </Badge>
-                        <Badge variant="outline" className="rounded-full px-2.5 py-1">
-                            {car.km_driven.toLocaleString()} km
+                        <Badge variant="outline" className="text-xs rounded-full px-2.5 py-1">
+                            {car.fuel_type}
+                        </Badge>
+                        <Badge variant="outline" className="text-xs rounded-full px-2.5 py-1">
+                            {car.registration_year}
                         </Badge>
                     </div>
                     <div className="mt-3">
-                        <span className="text-xl font-bold">
-                            ₹{car.discount_price.toLocaleString()}
-                        </span>
-
-                        <span className="line-through ml-2 text-gray-400">
-                            ₹{car.original_price.toLocaleString()}
-                        </span>
+                        <Button className="w-full bg-slate-900 text-white hover:">
+                            Book Now
+                        </Button>
+                    </div>
+                    <div className="mt-4 flex items-center justify-between gap-2 pt-2 text-sm text-slate-500 border-t ">
+                        <p className="text-xs">Is this car a good match for you?</p>
+                        <div className="flex items-center gap-2">
+                            <ThumbsUp className="w-4 h-4 cursor-pointer hover:text-green-500 transition" /> <span>Yes</span>
+                            <ThumbsDown className="w-4 h-4 cursor-pointer hover:text-red-500 transition" /> <span>No</span>
+                        </div>
                     </div>
                 </CardContent>
             </Card>
